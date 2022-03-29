01db92010d7cbf65e588682c31513ba12233ad96
Suhas Nidgundi <suhasnidgundi@gmail.com>
2022-03-29 21:51:11 +0530

    Use of String and Arithmetic Function in SQl

---
+++ Tutorial 10/Tutorial 10.sql
@@ -0,0 +1,10 @@
+use suveesoft;
+show tables;
+select name from student;
+select percentage from student;
+
+select upper('suhasanandnidgundi'), length('suhasanandnidgundi') from student;
+select concat('sundar', 'pichai') from student;
+
+select sqrt(4) from student;
+select name, round(percentage) from student;
