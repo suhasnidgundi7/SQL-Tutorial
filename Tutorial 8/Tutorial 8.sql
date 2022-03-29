1cc1bee839a0c8be4f1201ae5f8ea3ce227fcec4
Suhas Nidgundi <suhasnidgundi@gmail.com>
2022-03-29 21:03:05 +0530

    Use of operator in SQL Queries

---
+++ Tutorial 8/Tutorial 8.sql
@@ -0,0 +1,6 @@
+use suveesoft;
+desc student;
+select * from student;
+
+update student set name="suhasanandnidgundi" where enrollment = 2000400081;
+delete from student where enrollment = 2000400082;
